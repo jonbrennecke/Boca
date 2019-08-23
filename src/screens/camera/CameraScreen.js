93cd4e52cf82aeb225c31923c64f30d09862a3ca
Jon Brennecke <jpbrennecke@gmail.com>
2019-08-23 11:44:23 -0700

    upgraded react-native-camera

--- src/screens/camera/CameraScreen.js
+++ src/screens/camera/CameraScreen.js
@@ -126,14 +126,15 @@ export const CameraScreen: ComponentType<
         onRequestCameraPermissions={requestCameraPermissions}
       >
         <View style={styles.cameraWrap}>
-          {/* <Camera
+          <Camera
             style={styles.absoluteFill}
             ref={cameraRef}
             cameraPosition={cameraPosition}
             previewMode={isDepthPreviewEnabled ? 'depth' : 'portraitMode'}
             resizeMode="scaleAspectWidth"
             blurAperture={blurAperture}
-          /> */}
+            isPaused={isReviewModalVisible}
+          />
           <CameraFocusArea
             style={styles.absoluteFill}
             onRequestFocus={point => {
