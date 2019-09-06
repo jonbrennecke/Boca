d87239d3acdcfeff7e2415d573138facf75d0b6d
Jon Brennecke <jpbrennecke@gmail.com>
2019-09-06 14:38:18 -0700

    added disabled state to thumbnail icon

--- src/screens/camera/ThumbnailButton.js
+++ src/screens/camera/ThumbnailButton.js
@@ -14,10 +14,14 @@ export type ThumbnailButtonProps = {
 };
 
 const styles = {
-  thumbnail: {
+  container: (disabled: boolean) => ({
     borderWidth: 1,
     borderColor: Colors.solid.white,
     borderRadius: Units.extraSmall,
+    opacity: disabled ? 0.75 : 1,
+    overflow: 'hidden',
+  }),
+  thumbnail: {
     flex: 1,
   },
 };
@@ -27,7 +31,7 @@ export const ThumbnailButton: SFC<ThumbnailButtonProps> = ({
   assetID,
   onPress,
 }: ThumbnailButtonProps) => (
-  <TouchableOpacity style={style} onPress={onPress}>
+  <TouchableOpacity style={[styles.container(!assetID), style]} onPress={onPress} disabled={!assetID}>
     {assetID && <Thumbnail assetID={assetID} style={styles.thumbnail} />}
   </TouchableOpacity>
 );
