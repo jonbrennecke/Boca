9006ca8c09d74fa49e8715f076b11f3bb13aae7f
Jon Brennecke <jpbrennecke@gmail.com>
2019-09-07 11:53:47 -0700

    formatting

--- src/screens/camera/cameraScreenState.js
+++ src/screens/camera/cameraScreenState.js
@@ -193,9 +193,7 @@ export function wrapWithCameraScreenState<
           {
             initializationStatus: 'loaded',
           },
-          () => {
-            SplashScreen.hide();
-          }
+          () => SplashScreen.hide()
         );
       }
     }
