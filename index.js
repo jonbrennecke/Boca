e2fa59695b307e07db07ff7de52d9d7dd6249fb5
Jon Brennecke <jpbrennecke@gmail.com>
2019-09-07 13:33:54 -0700

    added install code for Sentry

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 import { YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { Navigation } from 'react-native-navigation';
+import { Sentry } from 'react-native-sentry';
 
 import createStore from './src/redux/store';
 import { registerScreens, setRoot } from './src/screens';
+
+Sentry.config('https://e40bc6d54e9b46edaffec9737da5fa86@sentry.io/1584983').install();
 
 YellowBox.ignoreWarnings([
   'Require cycle:', // NOTE: this hides a warning from the 'core-decorators' package
